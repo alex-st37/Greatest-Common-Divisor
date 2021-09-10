6c06b31b0925b1c0cb0070cf2d1856bd6323f89b
alex-st37 <90447306+alex-st37@users.noreply.github.com>
2021-09-10 13:22:04 +0300

    The Main Class of the program.

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Please enter the first number: ");
         int x = scanner.nextInt();
         scanner.nextLine();
+        
         System.out.println("Please enter the second number: ");
         int y = scanner.nextInt();
         scanner.nextLine();
+        
         int gcd = GreatestCommonDivisor.getGreatestCommonDivisor(x, y);
         System.out.println("The Greatest Common Divisor of " + x + " and " + y + " is " + gcd);
+        
         System.out.println("Press any key to exit: ");
         scanner.nextLine();
     }
